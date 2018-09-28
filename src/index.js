ff91c9b04c5ca8cd46d3f3eeb92acf3bc6a5bbbd
Kristalys <kristalysogr@gmail.com>
2018-09-28 21:46:39 +0300

    result-additional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = str.split('');
+  let result = [];
+
+  if (arr.length % 2 === 1) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] === bracketsConfig[j][1] && result[result.length - 1] === bracketsConfig[j][0]) {
+        result.pop();
+      }
+      else if (arr[i] === bracketsConfig[j][0]){
+        result.push(arr[i]);
+      }
+      
+    }   
+  }
+  if (result == "") {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
